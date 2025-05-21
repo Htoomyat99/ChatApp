38eae9ba34b0eccb1cd69371434e898e793a02df
Htoomyat99 <htoomyat.20399@gmail.com>
2025-05-21 23:56:15 +0630

    dark theme, light theme animated

--- app/animateFour.tsx
+++ app/animateFour.tsx
@@ -1,12 +1,49 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Switch } from "react-native";
+import Animated from "react-native-reanimated";
 
-const animateFour = () => {
+const Colors = {
+  dark: {
+    background: "#1E1E1E",
+    circle: "#252525",
+    text: "#F8F8F8",
+  },
+  light: {
+    background: "#F8F8F8",
+    circle: "#FFF",
+    text: "#1E1E1E",
+  },
+};
+
+const SWITCH_TRACK_COLOR = {
+  true: "rgba(256, 0, 256, 0.2)",
+  false: "rgba(0, 0, 0, 0.1)",
+};
+
+type Theme = "dark" | "light";
+
+const AnimateFour = () => {
+  const [theme, setTheme] = useState<Theme>("light");
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Text>animateFour</Text>
-    </SafeAreaView>
+    <Animated.View style={styles.container}>
+      <Switch
+        value={theme === "dark"}
+        onValueChange={(value) => setTheme(value ? "dark" : "light")}
+        trackColor={SWITCH_TRACK_COLOR}
+        thumbColor={"violet"}
+      />
+    </Animated.View>
   );
 };
 
-export default animateFour;
+export default AnimateFour;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
